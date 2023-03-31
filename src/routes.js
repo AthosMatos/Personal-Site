05e48014023ac6874e63ac9a77e55064c9aac988
Athos Matos <ladiesman217.as@gmail.com>
2023-03-31 02:09:06 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,8 @@
-import { useSelector } from "react-redux"
 import { Route,BrowserRouter, Routes } from "react-router-dom"
 import Organizer from "./Screens/Organizer"
 
 const MyRoutes = () =>
 {
-    const backColor = useSelector((state)=>state.TempBackColor.BackColor)
-
     return(
         <BrowserRouter>
             <Routes>
